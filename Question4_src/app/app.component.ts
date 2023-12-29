fda74f2da4f68524d402f7c6d34a4dc9c92d70ec
KordeAshwini <132747365+KordeAshwini@users.noreply.github.com>
2023-12-29 17:04:58 +0530

    Assignment 8

---
+++ Question4_src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent 
+{
+  title = 'EventListenerToTextbox';
+  
+  public Text : string = "";
+
+  public DisplayText(value : string)
+  {
+    this.Text = value;
+  }
+
+}
